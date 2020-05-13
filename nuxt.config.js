9ca244650a38a61a358d7c5586b53ea03355fece
lulu <xinhua.lulu@gmail.com>
2020-05-13 16:54:56 +0800

    引入 bootstrap.css 和 scss 環境建置

--- nuxt.config.js
+++ nuxt.config.js
@@ -23,7 +23,8 @@ export default {
   ** Global CSS
   */
   css: [
-    'element-ui/lib/theme-chalk/index.css'
+    '~assets/styles/bootstrap.css',
+    { src: '~assets/styles/index.scss', lang: 'scss' }
   ],
   /*
   ** Plugins to load before mounting the App
