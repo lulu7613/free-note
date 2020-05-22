15b542afc3f22c8cb378af82163dd0570ceaccb3
lulu <xinhua.lulu@gmail.com>
2020-05-22 15:46:36 +0800

    定義 axios response 項目

--- plugins/axios.js
+++ plugins/axios.js
@@ -4,4 +4,15 @@ const service = axios.create({
   baseURL: 'http://localhost:9527'
 })
 
+service.interceptors.response.use(
+  response => {
+    console.log(response)
+    return response
+  },
+  error => {
+    console.log('報錯了啦!!', error)
+    return Promise.reject(error)
+  }
+)
+
 export default service
