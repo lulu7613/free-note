7ece3ebb03a67728fe0cf0514054a72b6efa1dc8
lulu <xinhua.lulu@gmail.com>
2020-05-25 10:46:21 +0800

    新增 Navbar 元件、完成 select 切換樣式

---
+++ components/Navbar/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="py-4">
+    <div>
+      <el-select v-show="appMode === 'night'" class="select" popper-class="select-option" v-model="sortBy">
+        <el-option label="依照時間(由新到舊)" value="desc" />
+        <el-option label="依照時間(由舊到新)" value="asc" />
+      </el-select>
+      <el-select v-show="appMode === 'sunny'" class="select" v-model="sortBy">
+        <el-option label="依照時間(由新到舊)" value="desc" />
+        <el-option label="依照時間(由舊到新)" value="asc" />
+      </el-select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      sortBy: 'desc',
+    }
+  },
+
+  computed: {
+    appMode() {
+      return this.$store.state.appMode
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+@import '~assets/styles/variables.scss';
+
+  .select {
+    .el-input {
+      width: 25rem;
+    }
+
+    .el-input--suffix .el-input__inner {
+      border: 0;
+      font-family: inherit;
+    }
+  }
+
+  .select-option {
+    .el-scrollbar {
+      background-color: $--color-dark;
+    }
+    .el-select-dropdown__item.hover, .el-select-dropdown__item:hover {
+      background-color: $--color-black;
+    }
+  }
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,16 @@
 <template>
-  <div>
-    所有筆記
+  <div class="container">
+    <Navbar/>
+    <div>123</div>
   </div>
 </template>
 
 <script>
+import Navbar from '~/components/Navbar/index.vue'
 
 export default {
   components: {
+    Navbar
   },
 
   computed: {
@@ -28,5 +31,11 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+  :root {
+  --padding-width: 8vw;
+  }
+  .container {
+    padding: 0 var(--padding-width);
+  }
 </style>
